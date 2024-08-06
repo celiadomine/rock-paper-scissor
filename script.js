a4392988f591cf2b5567f46f3fa82e7d24cf8490
Célia Dominé <celia.domine@meteoblue.com>
2024-08-06 15:40:36 +0200

    calculate winner :)))

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-let chosenMove;
+let playerMove;
  
-function resetMove(){
-    chosenMove = 0;
-}
-
 document.getElementById('rockButton').addEventListener('click', function() {
-    chosenMove = 1;
+    playerMove = 'rock';
     document.getElementById('player').innerText = 'You played rock';
     selectMove();
+    calculateWinner();
 });
 
 document.getElementById('paperButton').addEventListener('click', function() {
-    chosenMove = 2;
+    playerMove = 'paper';
     document.getElementById('player').innerText = 'You played paper';
     selectMove();
+    calculateWinner();
 });
 
 document.getElementById('scissorsButton').addEventListener('click', function() {
-    chosenMove = 3;
+    playerMove = 'scissors';
     document.getElementById('player').innerText = 'You played scissors';
     selectMove();
+    calculateWinner();
 });
 
 function selectMove(){
     const randomNumber = Math.floor(Math.random() * 3); // random 0, 1 or 2 
     if (randomNumber === 0) {
         document.getElementById('computer').innerText = 'Computer played rock'; 
-        hisPlay = 'rock';
+        computerMove = 'rock';
     } else if (randomNumber === 1){
         document.getElementById('computer').innerText = 'Computer played paper'; 
-        hisPlay = 'paper';
+        computerMove = 'paper';
     } else {
         document.getElementById('computer').innerText = 'Computer played scissors'; 
-        hisPlay = 'scissors';
+        computerMove = 'scissors';
     }
 }
 
 function calculateWinner() {
-
+    if (playerMove === computerMove) {
+        document.getElementById('winner').innerText = "it's a tie :0 ";
+    } else if (
+        (playerMove === 'rock' && computerMove === 'scissors') ||
+        (playerMove === 'paper' && computerMove === 'rock') ||
+        (playerMove === 'scissors' && computerMove === 'paper')
+    ) {
+        document.getElementById('winner').innerText = "OMG YOU WINNNNNNNNNN!!!!";
+    } else {
+        document.getElementById('winner').innerText = "computer winss :((";
+    }
 }
 
